ff467c86a4de373050e4e90a345cd20d72f4a308
matthew rendely <matthew.rendely@gmail.com>
2023-06-24 22:20:14 -0700

    working basic linked list with tests

--- lib/linked_list.py
+++ lib/linked_list.py
@@ -1,5 +1,25 @@
+class Node:
+    def __init__(self, data, next_node = None):
+        self.data = data
+        self.next_node = next_node 
+
+    def __repr__(self):
+        return f'Node(data={self.data})'
+
 class LinkedList:
-  
-    @classmethod
-    def hello(cls):
-        return 'hello'
+    def __init__(self, head = None):
+        self.head = head
+
+    def append(self, node):
+        if self.head is None:
+            self.head = node 
+            return 
+
+        last_node = self.head 
+        while last_node.next_node:
+            last_node = last_node.next_node
+        
+        last_node.next_node = node 
+
+    def __repr__(self):
+        return f'LinkedList(head={self.head})'
--- lib/testing/test_linked_list.py
+++ lib/testing/test_linked_list.py
@@ -1,12 +1,37 @@
-from linked_list import LinkedList
+import pytest
+from linked_list import Node, LinkedList
+
 
 class TestLinkedList:
     '''Class LinkedList in linked_list.py'''
 
-    def test_hello(self):
-        '''Tests hello()'''
-        assert LinkedList.hello() == 'hello'
+    def test_Node(self):
+        '''Tests Node class'''
+        hello = Node('Hello')
+        assert isinstance(hello, Node)
+        assert hello.data == 'Hello'
     
-    def test_test_nothing(self):
-      '''Tests nothing'''
-      assert 'not hi' != 'hi'
+    def test_LinkedList(self):
+      '''Tests LinkedList class'''
+      hello = Node('Hello')
+      my_list = LinkedList(hello)
+      assert my_list.head == hello
+
+    def test_append(self):
+      '''Tests append()'''
+      first = Node('one')
+      second = Node('two')
+      my_list = LinkedList(first)
+      my_list.append(second)
+      assert first.next_node == second
+
+    def test_repr(self, capsys):
+      '''tests the __repr__'''
+      first = Node('one')
+      print(first)
+      out, err = capsys.readouterr()
+      assert out == 'Node(data=one)\n'
+      my_list = LinkedList(first)
+      print(my_list)
+      out, err = capsys.readouterr()
+      assert out == 'LinkedList(head=Node(data=one))\n'
